914830b87063b00802663c0e7fa65f7528fdfbab
FahimNuman87 <fahiomnuman87@gmail.com>
2024-03-04 11:18:41 +0600

    last update slider2

--- ecommerce/src/components/Slider2/page.jsx
+++ ecommerce/src/components/Slider2/page.jsx
@@ -1,33 +1,35 @@
+/* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
 // Mark the file as a client-side component
 'use client'
 import React, { useEffect } from 'react'
 import Glide from '@glidejs/glide'
 import Posts from '../CategoryByCare/page'
-import HealthCheckup from '../HealthCheckup/page'
+
 
 const Slider2 = () => {
   useEffect(() => {
-    
-    const slider = new Glide('.glide-03', {
-      type: 'carousel',
-      focusAt: 'center',
-      perView: 6,
-      autoplay: 3000,
+    const slider = new Glide(".glide-02", {
+      type: "carousel",
+      focusAt: "center",
+      perView: 3,
+      autoplay: 3500,
       animationDuration: 700,
       gap: 24,
-      breakpoints: {
-        1024: {
-          perView: 3,
-        },
-        640: {
-          perView: 3,
+      classNames: {
+        nav: {
+          active: "[&>*]:bg-wuiSlate-700",
         },
       },
-    })
-
-   
-    slider.mount()
+      breakpoints: {
+        1024: {
+          perView: 2,
+        },
+        640: {
+          perView: 1,
+        },
+      },
+    }).mount()
 
     
     return () => {
@@ -37,59 +39,18 @@ const Slider2 = () => {
   }, [])
   return (
     <>
-      <div className="glide-03 relative w-full">
+     <div className="bg-gray-50 py-5 md:px-12 px-2 flex justify-between items-center rounded-md ">
+   
+   <div className=" w-full rounded-box  z-10 relative rounded-md border ">
+   <div className=" flex whitespace-no-wrap ">
+     <div className="glide-02 relative w-full">
+        {/*    <!-- Slides --> */}
+       
         <div className="overflow-hidden " data-glide-el="track">
           <ul className=" whitespace-no-wrap flex-no-wrap [backface-visibility: hidden] [transform-style: preserve-3d] [touch-action: pan-Y] [will-change: transform] relative flex w-full overflow-hidden p-0">
-           <HealthCheckup/>
+            <Posts/>
           </ul>
-        </div>
-        {/*    <!-- Controls --> */}
-        <div
-          className="absolute left-0 top-1/2 flex h-0 w-full items-center justify-between px-4 "
-          data-glide-el="controls"
-        >
-          <button
-            className="inline-flex h-8 w-8 items-center justify-center rounded-full border border-slate-700 bg-white/20 text-slate-700 transition duration-300 hover:border-slate-900 hover:text-slate-900 focus-visible:outline-none lg:h-12 lg:w-12"
-            data-glide-dir="<"
-            aria-label="prev slide"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="h-5 w-5"
-            >
-              <title>prev slide</title>
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
-              />
-            </svg>
-          </button>
-          <button
-            className="inline-flex h-8 w-8 items-center justify-center rounded-full border border-slate-700 bg-white/20 text-slate-700 transition duration-300 hover:border-slate-900 hover:text-slate-900 focus-visible:outline-none lg:h-12 lg:w-12"
-            data-glide-dir=">"
-            aria-label="next slide"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="h-5 w-5"
-            >
-              <title>next slide</title>
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-              />
-            </svg>
-          </button>
+       
         </div>
         {/*    <!-- Indicators --> */}
         <div
@@ -126,7 +87,9 @@ const Slider2 = () => {
           </button>
         </div>
       </div>
-      {/*<!-- End Carousel with indicators & controls inside --> */}
+      </div>
+      </div>
+      </div>
     </>
   )
 }
