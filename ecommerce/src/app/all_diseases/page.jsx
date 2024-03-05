cadcafb544253693c335eb30dea92ee92c42763f
Rafsun Jani Shuvo <rafsunshuvo@gmail.com>
2024-03-05 10:44:04 +0600

    all medicine and all disease style issue fixed

--- ecommerce/src/app/all_diseases/page.jsx
+++ ecommerce/src/app/all_diseases/page.jsx
@@ -5,29 +5,27 @@ export default async function Posts() {
 const getDiseases = await getAllDiseases();
   return (
     <div>
-      <ProductIndex/>
+      <ProductIndex />
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        {getDiseases.map(diseases =>
-      
-          <div key={diseases.id} className="card card-side bg-base-100 shadow-xl" style={{ height: '300px' }}>
-          <figure style={{ height: '60%', overflow: 'hidden' }}>
-            <img
-              style={{ objectFit: 'cover', width: '100%', height: '100%' }}
-              src="https:encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtELB6BLBdGNjRrLnSK3G8fkX9m4FVF3yWlsqhBG2HCmg-Leoh-OuFCscijxBeMc87ZMo&usqp=CAU"
-              alt=""
-            />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">{diseases.title}</h2>
-            <p>{diseases.description}</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Watch</button>
+        {getDiseases.map(disease =>
+          <div key={disease.id} className="card card-side bg-base-100 shadow-xl" style={{ height: '120px' }}>
+            <div className='w-1/3 flex'>
+              <figure >
+                <img
+                  style={{ objectFit: 'cover', width: '30%' }}
+                  src="https:encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtELB6BLBdGNjRrLnSK3G8fkX9m4FVF3yWlsqhBG2HCmg-Leoh-OuFCscijxBeMc87ZMo&usqp=CAU"
+                  alt=""
+                />
+              </figure>
             </div>
+            <div className="card-body w-2/3">
+              <div className="card-title text-sm">{disease.title}</div>
+            </div>
+
           </div>
-        </div> 
         )}
-        </div>
-       </div>  
+      </div>
+    </div>
 
   )
 }
