334752f3ed16711b3fcb6a306faea8f148baa59a
Rafsun Jani Shuvo <rafsunshuvo@gmail.com>
2024-03-14 11:05:36 +0600

    sidebar menu fixed in diseage details page , medicine name color added, company name added , generic to product page

--- ecommerce/src/app/brands/layout.jsx
+++ ecommerce/src/app/brands/layout.jsx
@@ -1,93 +1,41 @@
 'use client';
 import React from 'react';
-import Link from 'next/link';
-import { useState } from 'react';
-const Accordion = ({ title, items }) => {
-    const [isOpen, setIsOpen] = useState(false);
-
-    const handleToggle = () => {
-        setIsOpen(!isOpen);
-    };
-    return (
-        <div className={`collapse collapse-plus mb-4 ${isOpen ? 'open' : ''}`}>
-            <input type="checkbox" id={title} defaultChecked={isOpen} onChange={handleToggle} />
-            <div className="collapse-title text-xl font-medium">
-                <label htmlFor={title}>{title}</label>
-            </div>
-            {isOpen && (
-                <div className="collapse-content">
-                    <ul className="list-none">
-                        {items.map((item, index) => (
-                            <li key={index}>
-                                <Link href={item.link}>{item.label}</Link>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            )}
-        </div>
-    );
-};
 
 export default function Therapeuticlayout({ children }) {
-    const accordionData = [
-        {
-            title: 'Accordion 1',
-            items: [
-                { label: 'Human Insulin Premix', link: '/drug_therapeutic_classes/1' },
-                { label: 'Human Insulin Basal', link: '#' },
-            ],
-        },
-        {
-            title: 'Accordion 2',
-            items: [
-                { label: 'Human Insulin Premix', link: '/drug_therapeutic_classes/3' },
-                { label: 'Human Insulin Basal', link: '#' },
-            ],
-        },
-        {
-            title: 'Accordion 3',
-            items: [
-                { label: 'Human Insulin Premix', link: '#' },
-                { label: 'Human Insulin Basal', link: '#' },
-            ],
-        },
-    ];
+
     return (
         <>
             <div className="flex">
-                <div className="card bg-slate-50 p-4 w-1/5">
-                    <h1>Filter</h1>
+                <div className="card bg-slate-50 p-4 w-1/6">
+                    <h1 className='flex justify-center'>Filter</h1>
                     <hr />
+                    <h1 className='flex justify-center'>Product Form</h1>
                     <table className="table">
-                        <thead>
-                            <tr>
-                                <th colSpan="2"><h1>Product Form</h1></th>
-                            </tr>
-                        </thead>
                         <tbody>
                             <tr>
-                                <td><input type="checkbox" className="checkbox" /></td>
+                                <td><input type="checkbox" /></td>
                                 <td>Capsule</td>
+                                <td>3</td>
                             </tr>
                             <tr>
-                                <td><input type="checkbox" className="checkbox" /></td>
+                                <td><input type="checkbox" /></td>
                                 <td>Injection</td>
+                                <td>1</td>
                             </tr>
                             <tr>
-                                <td><input type="checkbox" className="checkbox" /></td>
+                                <td><input type="checkbox" /></td>
                                 <td>Powder</td>
+                                <td>5</td>
                             </tr>
                             <tr>
-                                <td><input type="checkbox" className="checkbox" /></td>
+                                <td><input type="checkbox" /></td>
                                 <td>Tablet</td>
+                                <td>1</td>
                             </tr>
                         </tbody>
                     </table>
-
                 </div>
-
-                <div className="w-4/5 p-4">
+                <div className="w-5/6 p-3">
                     {children}
                 </div>
             </div>
--- ecommerce/src/app/brands/page.jsx
+++ ecommerce/src/app/brands/page.jsx
@@ -7,39 +7,250 @@ const drugs_therapeutic = () => {
       <div className="grid grid-cols-1 mt-3 ml-4">
         <div>
           <h1 className='font-bold'>
-          Search results for Omez
+            Search results for Omez
           </h1>
         </div>
-     
-          <div key="" className="card card-side bg-base-100 shadow-xl flex w-full mb-2">
-            <div className="col-span-1 p-1 flex justify-items-center">
-              <figure>
-                <img
-                  style={{ width: '40px' }}
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtELB6BLBdGNjRrLnSK3G8fkX9m4FVF3yWlsqhBG2HCmg-Leoh-OuFCscijxBeMc87ZMo&usqp=CAU"
-                  alt=""
-                />
-              </figure>
-            </div>
-            <div className="card-body col-span-11 pl-3">
-              <div className="card-title text-xs font-bold flex justify-between">
-                <span>
-                    Omez Injection
-                </span>
-                <span>
-                    80 tk
-                </span>
-                <br />
-              </div>
-              <small>
-                Prescription Required
-              </small>
-              <div className="text-xs">
-                Strip Of 20 Capsules
-              </div>
-            </div>
+
+        <div key="" className="card card-side bg-base-100 shadow-xl flex w-full mb-2">
+          <div className="col-span-3 p-1 flex justify-items-center">
+            <figure>
+              <img
+                style={{ width: '80px' }}
+                src="https://onemg.gumlet.io/images/w_150,c_fit,h_150,q_auto,f_auto/hx2gxivwmeoxxxsc1hix/omez-injection.png"
+                alt=""
+              />
+            </figure>
           </div>
-        
+          <div className="card-body col-span-9 pl-3 flex justify-between">
+            <div className="card-title text-xs font-bold flex justify-between">
+              <span>
+                Omez Injection
+              </span>
+              <span>
+                MRP : 100tk
+              </span>
+            </div>
+            <small>
+              Vial Of 10 ml Injection
+            </small>
+            <div className="text-xs flex flex-col items-end">
+              <div className='line-through'> Tk 50 OFF </div>
+              <div className="font-semibold text-[#ff6f61] py-2">ADD</div>
+            </div>
+
+          </div>
+
+        </div>
+        <div key="" className="card card-side bg-base-100 shadow-xl flex w-full mb-2">
+          <div className="col-span-3 p-1 flex justify-items-center">
+            <figure>
+              <img
+                style={{ width: '80px' }}
+                src="https://onemg.gumlet.io/images/w_150,c_fit,h_150,q_auto,f_auto/hx2gxivwmeoxxxsc1hix/omez-injection.png"
+                alt=""
+              />
+            </figure>
+          </div>
+          <div className="card-body col-span-9 pl-3 flex justify-between">
+            <div className="card-title text-xs font-bold flex justify-between">
+              <span>
+                Omez Injection
+              </span>
+              <span>
+                MRP : 100tk
+              </span>
+            </div>
+            <small>
+              Vial Of 10 ml Injection
+            </small>
+            <div className="text-xs flex flex-col items-end">
+              <div className='line-through'> Tk 50 OFF </div>
+              <div className="font-semibold text-[#ff6f61] py-2">ADD</div>
+            </div>
+
+          </div>
+
+        </div>
+        <div key="" className="card card-side bg-base-100 shadow-xl flex w-full mb-2">
+          <div className="col-span-3 p-1 flex justify-items-center">
+            <figure>
+              <img
+                style={{ width: '80px' }}
+                src="https://onemg.gumlet.io/images/w_150,c_fit,h_150,q_auto,f_auto/hx2gxivwmeoxxxsc1hix/omez-injection.png"
+                alt=""
+              />
+            </figure>
+          </div>
+          <div className="card-body col-span-9 pl-3 flex justify-between">
+            <div className="card-title text-xs font-bold flex justify-between">
+              <span>
+                Omez Injection
+              </span>
+              <span>
+                MRP : 100tk
+              </span>
+            </div>
+            <small>
+              Vial Of 10 ml Injection
+            </small>
+            <div className="text-xs flex flex-col items-end">
+              <div className='line-through'> Tk 50 OFF </div>
+              <div className="font-semibold text-[#ff6f61] py-2">ADD</div>
+            </div>
+
+          </div>
+
+        </div>
+        <div key="" className="card card-side bg-base-100 shadow-xl flex w-full mb-2">
+          <div className="col-span-3 p-1 flex justify-items-center">
+            <figure>
+              <img
+                style={{ width: '80px' }}
+                src="https://onemg.gumlet.io/images/w_150,c_fit,h_150,q_auto,f_auto/hx2gxivwmeoxxxsc1hix/omez-injection.png"
+                alt=""
+              />
+            </figure>
+          </div>
+          <div className="card-body col-span-9 pl-3 flex justify-between">
+            <div className="card-title text-xs font-bold flex justify-between">
+              <span>
+                Omez Injection
+              </span>
+              <span>
+                MRP : 100tk
+              </span>
+            </div>
+            <small>
+              Vial Of 10 ml Injection
+            </small>
+            <div className="text-xs flex flex-col items-end">
+              <div className='line-through'> Tk 50 OFF </div>
+              <div className="font-semibold text-[#ff6f61] py-2">ADD</div>
+            </div>
+
+          </div>
+
+        </div>
+        <div key="" className="card card-side bg-base-100 shadow-xl flex w-full mb-2">
+          <div className="col-span-3 p-1 flex justify-items-center">
+            <figure>
+              <img
+                style={{ width: '80px' }}
+                src="https://onemg.gumlet.io/images/w_150,c_fit,h_150,q_auto,f_auto/hx2gxivwmeoxxxsc1hix/omez-injection.png"
+                alt=""
+              />
+            </figure>
+          </div>
+          <div className="card-body col-span-9 pl-3 flex justify-between">
+            <div className="card-title text-xs font-bold flex justify-between">
+              <span>
+                Omez Injection
+              </span>
+              <span>
+                MRP : 100tk
+              </span>
+            </div>
+            <small>
+              Vial Of 10 ml Injection
+            </small>
+            <div className="text-xs flex flex-col items-end">
+              <div className='line-through'> Tk 50 OFF </div>
+              <div className="font-semibold text-[#ff6f61] py-2">ADD</div>
+            </div>
+
+          </div>
+
+        </div>
+        <div key="" className="card card-side bg-base-100 shadow-xl flex w-full mb-2">
+          <div className="col-span-3 p-1 flex justify-items-center">
+            <figure>
+              <img
+                style={{ width: '80px' }}
+                src="https://onemg.gumlet.io/images/w_150,c_fit,h_150,q_auto,f_auto/hx2gxivwmeoxxxsc1hix/omez-injection.png"
+                alt=""
+              />
+            </figure>
+          </div>
+          <div className="card-body col-span-9 pl-3 flex justify-between">
+            <div className="card-title text-xs font-bold flex justify-between">
+              <span>
+                Omez Injection
+              </span>
+              <span>
+                MRP : 100tk
+              </span>
+            </div>
+            <small>
+              Vial Of 10 ml Injection
+            </small>
+            <div className="text-xs flex flex-col items-end">
+              <div className='line-through'> Tk 50 OFF </div>
+              <div className="font-semibold text-[#ff6f61] py-2">ADD</div>
+            </div>
+
+          </div>
+
+        </div>
+        <div key="" className="card card-side bg-base-100 shadow-xl flex w-full mb-2">
+          <div className="col-span-3 p-1 flex justify-items-center">
+            <figure>
+              <img
+                style={{ width: '80px' }}
+                src="https://onemg.gumlet.io/images/w_150,c_fit,h_150,q_auto,f_auto/hx2gxivwmeoxxxsc1hix/omez-injection.png"
+                alt=""
+              />
+            </figure>
+          </div>
+          <div className="card-body col-span-9 pl-3 flex justify-between">
+            <div className="card-title text-xs font-bold flex justify-between">
+              <span>
+                Omez Injection
+              </span>
+              <span>
+                MRP : 100tk
+              </span>
+            </div>
+            <small>
+              Vial Of 10 ml Injection
+            </small>
+            <div className="text-xs flex flex-col items-end">
+              <div className='line-through'> Tk 50 OFF </div>
+              <div className="font-semibold text-[#ff6f61] py-2">ADD</div>
+            </div>
+
+          </div>
+
+        </div>
+        <div key="" className="card card-side bg-base-100 shadow-xl flex w-full mb-2">
+          <div className="col-span-3 p-1 flex justify-items-center">
+            <figure>
+              <img
+                style={{ width: '80px' }}
+                src="https://onemg.gumlet.io/images/w_150,c_fit,h_150,q_auto,f_auto/hx2gxivwmeoxxxsc1hix/omez-injection.png"
+                alt=""
+              />
+            </figure>
+          </div>
+          <div className="card-body col-span-9 pl-3 flex justify-between">
+            <div className="card-title text-xs font-bold flex justify-between">
+              <span>
+                Omez Injection
+              </span>
+              <span>
+                MRP : 100tk
+              </span>
+            </div>
+            <small>
+              Vial Of 10 ml Injection
+            </small>
+            <div className="text-xs flex flex-col items-end">
+              <div className='line-through'> Tk 50 OFF </div>
+              <div className="font-semibold text-[#ff6f61] py-2">ADD</div>
+            </div>
+
+          </div>
+
+        </div>
       </div>
     </div>
   )
